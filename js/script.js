fd066b7e219f9c4022a6bf9c126fc19d80aee91b
PabloMagna <pablo.magnavachi@alumnos.frgp.utn.edu.ar>
2023-05-18 21:57:23 -0300

    Agrega archivos de la página web

--- js/script.js
+++ js/script.js
@@ -46,3 +46,38 @@ ScrollReveal().reveal('.home-content h1, .about-img'
     , { origin: 'left' });
 ScrollReveal().reveal('.home-content p, .about-content'
     , { origin: 'rigth' });
+
+
+//contact messagge
+
+var form = document.getElementById("my-form");
+
+async function handleSubmit(event) {
+    event.preventDefault();
+    var status = document.getElementById("my-form-status");
+    var data = new FormData(event.target);
+    fetch(event.target.action, {
+        method: form.method,
+        body: data,
+        headers: {
+            'Accept': 'application/json'
+        }
+    }).then(response => {
+        if (response.ok) {
+            status.innerHTML = "Thanks for your submission!";
+            form.reset()
+        } else {
+            response.json().then(data => {
+                if (data.errors) {
+                    status.innerHTML = data.errors.map(error => error.message).join(", ");
+                } else {
+                    status.innerHTML = "Oops! There was a problem submitting your form";
+                }
+            });
+        }
+    }).catch(error => {
+        status.innerHTML = "Oops! There was a problem submitting your form";
+    });
+}
+
+form.addEventListener("submit", handleSubmit);
